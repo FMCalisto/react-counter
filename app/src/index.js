e75bd3249b79687cf8a7e4a6d83fc7372cd6ae6c
Francisco Maria Calisto <email@franciscocalisto.me>
2017-03-07 22:19:19 +0000

    [BEGIN] Start Here

--- app/src/index.js
+++ app/src/index.js
@@ -2,30 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var Counter = React.createClass({
-  incrementCount: function(){
-    this.setState({
-      count: this.state.count + 1
-    });
-  },
-  getInitialState: function(){
-     return {
-       count: 0
-     }
-  },
-  render: function(){
-    return (
-      <div className="counter">
-        <h1>{this.state.count}</h1>
-        <button type="button" onClick={this.incrementCount}>Increment</button>
-      </div>
-    );
-  }
-});
 
-// React.renderComponent(<Counter/>, document.getElementById('mount-point'));
-
-ReactDOM.render(
-  <Counter/>,
-  document.getElementById('mount-point')
-);
